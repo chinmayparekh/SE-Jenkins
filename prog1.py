c5b88eab75c293ec5331c364575b2087d3e67090
Chinmay Parekh <76399431+chinmayparekh@users.noreply.github.com>
2022-11-24 18:44:15 +0530

    created all programs

---
+++ prog1.py
@@ -0,0 +1,6 @@
+def factorial(n):
+    fact = 1
+    for i in range(n+1):
+        fact*=i
+    return fact
+
---
+++ prog2.py
@@ -0,0 +1,6 @@
+def summation(n):
+    sum = 0
+    for i in range(n+1):
+        sum+=i
+    return summation
+
